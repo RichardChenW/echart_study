16c852d66c52fafb717d060f6f258ff34180d7bd
richard-git <402822848@qq.com>
2023-01-04 20:11:02 +0800

    202314

--- vision/src/components/Seller.vue
+++ vision/src/components/Seller.vue
@@ -2,48 +2,78 @@
 
 <template>
     <div class="com-container">
-        <div class="com-chart" ref="seller_ref" style="width:1000px;height:1000px"></div>
+        <div class="com-chart" ref="seller_ref" style="width:1000px;height:600px"></div>
     </div>
 </template>
 
 <script>
+
 export default {
     name:"seller",
     data() {
         return {
             chartInstance:null,
-            allData:null
+            allData:null, // 服务器返回的数据
+            currentPage:1, // 当前显示的页数
+            totalPage:0, // 一共有多少页
+            timerId:null
         }
     },
     methods: {
 
         // 1. 初始化echartInstance
         initChart(){
-            this.chartInstance  = this.$echarts.init(this.$refs.seller_ref)
+            this.chartInstance  = this.$echarts.init(this.$refs.seller_ref,"chalk")
             this.getData()
+            this.chartInstance.on("mouseover",()=>{
+                clearInterval(this.timerId)
+            }),
+            this.chartInstance.on("mouseout",()=>{
+                this.startInterval()
+            })
         },
 
         // 2. 获取服务器数据
         async getData(){
            const {data:res} = await this.$http.get("http://127.0.0.1:8888/api/seller")
            this.allData = res
+
+           // 得到数据就可以得到页面总数量
+            this.totalPage = Math.ceil(this.allData.length/5)
+
+           this.allData.sort((a,b)=>{
+                return a.value - b.value
+           })
+           console.log(this.allData)
            this.updateChart()
+           this.startInterval()
         },
 
         // 3. 更新图表
         updateChart(){
-            const sellerName = this.allData.map((item)=>{
+            let start = (this.currentPage - 1)*5
+            let end = this.currentPage * 5
+            const showData = this.allData.slice(start,end)
+
+            const sellerName = showData.map((item)=>{
                 return item.name
             })
-            const sellerValue = this.allData.map((item)=>{
+            const sellerValue = showData.map((item)=>{
                 return item.value
             })
 
-            console.log(sellerName,sellerValue)
+            // console.log(sellerName,sellerValue)
             
             const option = {
                 title:{
-                    text:"测试"
+                    text:"商家销售统计",
+                    textStyle:{
+                        fontSize:35
+                    }
+                },
+                grid:{
+                    containLable:true,
+                    left:"10%"
                 },
                 xAxis:{
                     type:"value"
@@ -55,19 +85,45 @@ export default {
                 series:[
                     {
                         type:"bar",
-                        data:sellerValue
+                        data:sellerValue,
+                        barWidth:40,
+                        showBackground: true,
+                        label:{
+                            show:true,
+                            position:"right",
+                            textStyle:{
+                                color:"white"
+                            }
+                        },
+                        itemStyle:{
+                            borderRadius:5
+                        }
                     }
-                ]
+                ],
             }
             this.chartInstance.setOption(option)
         },
+        startInterval(){
+            this.timerId = setInterval(()=>{
+                this.currentPage++
+                if(this.currentPage > this.totalPage){
+                    this.currentPage = 1
+                }
+                this.updateChart()
+            },1000)
+        }
     },
     mounted(){
         this.initChart()
+    },
+    destroyed(){
+        clearInterval(this.timerId)
     }
 }
 </script>
 
 <style>
-
+    canvas{
+        border-radius: 3%;
+    }
 </style>
